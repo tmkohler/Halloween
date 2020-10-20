7e256e277858e27c4cf38fdfda8552576e7e6f69
tmkohler <tmkohler@sbcglobal.net>
2020-10-19 22:05:04 -0400

    black ambient light
    black ambient light added, assets are illuminated

---
+++ Week5Halloween/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@J:\Theresa Kohler\My Documents\Unity\WorldBuilding\Week5Lighting\Halloween\Week5Halloween\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1586409796163076877.json
